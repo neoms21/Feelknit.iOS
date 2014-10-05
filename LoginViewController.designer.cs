51f450dadd9b4122815b21f5640327ece77c744a
neoms21 <neoms21@gmail.com>
2014-10-05 01:04:58 +0100

    Initial creation

---
+++ LoginViewController.designer.cs
@@ -0,0 +1,53 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace Feelknit
+{
+	[Register ("LoginViewController")]
+	partial class LoginViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton LoginButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField Password { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton RegistrationButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField UserName { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (LoginButton != null) {
+				LoginButton.Dispose ();
+				LoginButton = null;
+			}
+			if (Password != null) {
+				Password.Dispose ();
+				Password = null;
+			}
+			if (RegistrationButton != null) {
+				RegistrationButton.Dispose ();
+				RegistrationButton = null;
+			}
+			if (UserName != null) {
+				UserName.Dispose ();
+				UserName = null;
+			}
+		}
+	}
+}
