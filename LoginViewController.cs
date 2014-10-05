885a5b7e492fac485d9a022013555c7c7a600c36
Manoj Sethi <neoms21@gmail.com>
2014-10-05 01:18:52 +0100

    Added registration Controller

--- LoginViewController.cs
+++ LoginViewController.cs
@@ -10,5 +10,22 @@ namespace Feelknit
 		public LoginViewController (IntPtr handle) : base (handle)
 		{
 		}
+
+	    public override void ViewDidLoad()
+	    {
+	        base.ViewDidLoad();
+
+
+            RegistrationButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                // Launches a new instance of CallHistoryController
+                var registration = this.Storyboard.InstantiateViewController(typeof(RegistrationViewController).Name)
+                    as RegistrationViewController;
+                if (registration != null)
+                {
+                    this.NavigationController.PushViewController(registration, true);
+                }
+            };
+	    }
 	}
 }
---
+++ RegistrationViewController.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace Feelknit
+{
+	partial class RegistrationViewController : UIViewController
+	{
+		public RegistrationViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
