49b81b0f58789a53764c2d600bc83fe8fc6508db
neoms21 <neoms21@gmail.com>
2014-10-13 22:17:11 +0100

    added users feeling controller

---
+++ UserFeelingsController.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace Feelknit
+{
+	partial class UserFeelingsController : UIViewController
+	{
+		public UserFeelingsController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
---
+++ UserFeelingsController.designer.cs
@@ -0,0 +1,37 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace Feelknit
+{
+	[Register ("UserFeelingsController")]
+	partial class UserFeelingsController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CreateNewFeelingButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITableView UserFeelingsTable { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (CreateNewFeelingButton != null) {
+				CreateNewFeelingButton.Dispose ();
+				CreateNewFeelingButton = null;
+			}
+			if (UserFeelingsTable != null) {
+				UserFeelingsTable.Dispose ();
+				UserFeelingsTable = null;
+			}
+		}
+	}
+}
